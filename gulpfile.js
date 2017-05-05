4f72b4e1f03f72ee533d46fdf3cfccafb62c0c63
zhangHao <740586220@qq.com>
2017-05-05 14:57:50 +0800

    1.3

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+
+var paths = {
+  sass: ['./scss/**/*.scss']
+};
+
+
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('watch', ['sass'], function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
+
+
+gulp.task('copyImg', function() {
+  return gulp.src('www/img/*.*')
+            .pipe(gulp.dest('bulid/img'))
+});
+gulp.task('copyLib', function() {
+  return gulp.src('www/lib/**/*.*')
+              .pipe(gulp.dest('bulid/lib'))
+});
+gulp.task('copy',['copyImg', 'copyLib']);
+
+gulp.task('index', function() {
+  return gulp.src('www/index.html')
+              .pipe(useref())
+              .pipe(gulpif('*.js', uglify()))
+              .pipe(gulpif('*.css', minifyCss()))
+				      .pipe(gulp.dest('bulid'))
+})
+
+gulp.task('clean', function() {
+  return gulp.src('build')
+              .pipe(vinylPaths(del))
+})
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('copy', 'index');
+});
+
+
+
+
+
